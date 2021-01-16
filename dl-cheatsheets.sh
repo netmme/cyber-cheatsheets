65c5b0797623e36a30ba1f0da7f3cc60473b3ae3
Netmme <38214932+netmme@users.noreply.github.com>
2021-01-16 14:21:31 +0100

    Fixing + Clamav

--- dl-cheatsheets.sh
+++ dl-cheatsheets.sh
@@ -2,17 +2,17 @@ echo "Preaparing for file transfer..."
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied, using tost directory"
-    doc_directory="tost"
+    destination_directory="tost"
 else
-    doc_directory=$1
+    destination_directory=$1
 fi
 
-mkdir -p ${doc_directory}
-#rm -rf $1/*
-mkdir -p $1/cheatsheets/sans
-cp cheatsheets_list/missing_sans.list $1/cheatsheets/sans
-cp cheatsheets_list/misc.list $1/cheatsheets/
-cd $1
+mkdir -p ${destination_directory}
+rm -rf ${destination_directory}/*
+mkdir -p ${destination_directory}/cheatsheets/sans
+cp cheatsheets_list/missing_sans.list ${destination_directory}/cheatsheets/sans
+cp cheatsheets_list/misc.list ${destination_directory}/cheatsheets/
+cd ${destination_directory}
 
 echo "Downloading github..."
 git clone https://github.com/BushidoUK/Open-source-tools-for-CTI
@@ -35,17 +35,18 @@ mv site owasp-cheatsheets
 cd sans
 wget https://digital-forensics.sans.org/community/cheat-sheets?msc=Cheat+Sheet+Blog
 cat 'cheat-sheets?msc=Cheat+Sheet+Blog' | grep -o -P "\/media[^\>]*?\.pdf.*?\'" > ready_to_sed
-cp ready_to_sed ready1
 sed -i -e 's/^/https:\/\/digital-forensics.sans.org\//' ready_to_sed
-cp ready_to_sed ready2
-
 wget https://www.sans.org/blog/the-ultimate-list-of-sans-cheat-sheets/
 cat index.html | grep -o -P "https:[^\>]*?\.pdf.*?\"" >> ready_to_sed
 sed 's/.$//' ready_to_sed > seded
 cat missing_sans.list >> seded
 sort seded | uniq > all_sans_cheatsheets.list
 
-wget -i all_files
+wget -i all_sans_cheatsheets.list
 rm missing_sans.list index.html ready_to_sed seded 'cheat-sheets?msc=Cheat+Sheet+Blog'
+
+echo "Checking for compromised files..."
+cd ../..
+clamscan -r --bell -i ./
 
 echo "All files in place."
