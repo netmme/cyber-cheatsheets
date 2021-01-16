64a74389221abafcda40311f38e2175c56d97566
Netmme <38214932+netmme@users.noreply.github.com>
2021-01-16 13:54:39 +0100

    Nettoyage

--- dl-cheatsheets.sh
+++ dl-cheatsheets.sh
@@ -10,8 +10,8 @@ fi
 mkdir -p ${doc_directory}
 #rm -rf $1/*
 mkdir -p $1/cheatsheets/sans
-cp cheatsheets_list/missing_sans $1/cheatsheets/sans
-cp cheatsheets_list/misc $1/cheatsheets/
+cp cheatsheets_list/missing_sans.list $1/cheatsheets/sans
+cp cheatsheets_list/misc.list $1/cheatsheets/
 cd $1
 
 echo "Downloading github..."
@@ -42,10 +42,10 @@ cp ready_to_sed ready2
 wget https://www.sans.org/blog/the-ultimate-list-of-sans-cheat-sheets/
 cat index.html | grep -o -P "https:[^\>]*?\.pdf.*?\"" >> ready_to_sed
 sed 's/.$//' ready_to_sed > seded
-cat missing_sans >> seded
+cat missing_sans.list >> seded
 sort seded | uniq > all_sans_cheatsheets.list
 
 wget -i all_files
-rm missing_sans index.html ready_to_sed seded 'cheat-sheets?msc=Cheat+Sheet+Blog'
+rm missing_sans.list index.html ready_to_sed seded 'cheat-sheets?msc=Cheat+Sheet+Blog'
 
 echo "All files in place."
